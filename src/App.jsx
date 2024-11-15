3dd99707cfda31cf21b0ff87464c0c5199134203
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-11-15 19:41:48 +0200

    Remove code for Header - created a separate component for it

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans"
 import VanDetail from "./pages/VanDetail"
+import Layout from "./components/Layout"
 
 
 import "../server"
@@ -27,18 +28,13 @@ function App() {
 
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
         <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/about" element={<About />}/>
-          <Route path="/vans" element={<Vans />}/>
-          <Route path="/vans/:id" element={<VanDetail />} />
+            <Route element={<Layout />}>
+                <Route path="/" element={<Home />}/>
+                <Route path="/about" element={<About />}/>
+                <Route path="/vans" element={<Vans />}/>
+                <Route path="/vans/:id" element={<VanDetail />} />
+            </Route>
         </Routes>
       </BrowserRouter>
   )
