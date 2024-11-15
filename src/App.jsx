3aca789e5f33a03dae866f060035f1388b421ea6
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-11-15 19:55:11 +0200

    Import components and add routes

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,9 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans/Vans"
 import VanDetail from "./pages/Vans/VanDetail"
+import Dashboard from "./pages/Host/Dashboard"
+import Income from "./pages/Host/Income"
+import Reviews from "./pages/Host/Reviews"
 import Layout from "./components/Layout"
 
 
@@ -34,6 +37,9 @@ function App() {
                 <Route path="/about" element={<About />}/>
                 <Route path="/vans" element={<Vans />}/>
                 <Route path="/vans/:id" element={<VanDetail />} />
+                <Route path="/host" element={<Dashboard />} />
+                <Route path="/host/income" element={<Income />} />
+                <Route path="/host/reviews" element={<Reviews />} />
             </Route>
         </Routes>
       </BrowserRouter>
