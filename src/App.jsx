421759574d85d22824f32eb2ee132a4de36b6dfc
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-11-15 16:19:07 +0200

    Create router

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,28 @@
  * Review challenge: do all the CSS yourself based on the design
  * linked in the slides.
  */
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+
+import "./server"
 
 function App() {
 
   return (
-    <h1>Start here</h1>
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+        </Routes>
+      </BrowserRouter>
   )
 }
 
