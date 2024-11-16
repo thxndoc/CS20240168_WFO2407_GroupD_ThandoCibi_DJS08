5c64cc593b177e471c03218456244e9851722ebd
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-11-16 11:18:00 +0200

    Add host van routes

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,8 @@ import VanDetail from "./pages/Vans/VanDetail"
 import Dashboard from "./pages/Host/Dashboard"
 import Income from "./pages/Host/Income"
 import Reviews from "./pages/Host/Reviews"
-import HostVans from './pages/HostVans';
+import HostVans from './pages/Host/HostVans';
+import HostVanDetail from "./pages/Host/HostVanDetail"
 import Layout from "./components/Layout"
 import HostLayout from "./components/HostLayout"
 
@@ -44,6 +45,7 @@ function App() {
             <Route path="income" element={<Income />} />
             <Route path="reviews" element={<Reviews />} />
             <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetail />} />
           </Route>
         </Route>
       </Routes>
--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -18,13 +18,22 @@ export default function HostLayout() {
                 style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Dashboard
-                    </NavLink>
+                </NavLink>
+
                 <NavLink 
                 to="/host/income"
                 style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Income
                 </NavLink>
+
+                <NavLink 
+                to="/host/vans"
+                style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Vans
+                </NavLink>
+
                 <NavLink 
                 to="/host/reviews"
                 style={({ isActive }) => isActive ? activeStyles : null}
